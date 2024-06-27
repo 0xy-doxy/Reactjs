ba81ec39119c605999b19e9a75db12402dcadafa
Pratik Dattatray Suryawanshi <pratik45@Pratiks-MacBook-Air.local>
2024-06-27 20:15:15 +0530

    connection with mongodb atlas

---
+++ 12Blog/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container"
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+
+export {Header, Footer, Container, Logo, LogoutBtn}
